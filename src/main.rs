c05a5577ec7394c6c933128abff3fac6bfa688a7
kama <116267218+kama93i@users.noreply.github.com>
2026-02-13 09:45:20 +0100

    Created App struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode};
 use ratatui::{DefaultTerminal, Frame};
 
+struct App {
+    running: bool,
+    input: String,
+    messages: Vec<String>,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            running: true,
+            input: String::new(),
+            messages: vec!["Welcome to my app! (press 'q' to exit)".into()],
+        }
+    }
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?; // nice panic messages
     let terminal = ratatui::init(); // enters alternate screen + raw mode
@@ -11,8 +27,8 @@ fn main() -> Result<()> {
 }
 
 fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    loop {
-        // Step 1: Draw
+    let mut app = App::new();
+    while app.running {
         terminal.draw(|frame| render(frame))?;
 
         // Step 2: Wait for input
